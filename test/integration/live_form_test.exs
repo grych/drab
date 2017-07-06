be889a4b3bc4d4c5380f22b300d16c314ff7c3d3
Tomek Gryszkiewicz <grych@tg.pl>
2017-07-06 22:08:48 +0200

    test for form

---
+++ test/integration/live_form_test.exs
@@ -0,0 +1,40 @@
+defmodule DrabTestApp.LiveForm do
+  import Drab.Live
+  use DrabTestApp.IntegrationCase
+
+  defp form_index do
+    form_url(DrabTestApp.Endpoint, :form)
+  end
+
+  setup do
+    form_index() |> navigate_to()
+    find_element(:id, "page_loaded_indicator") # wait for the Drab to initialize
+    [socket: drab_socket()]
+  end
+
+  describe "Drab.Live" do
+    test "form should return initial values" do
+      socket = drab_socket()
+      assert peek(socket, :out) == %{}
+      click_and_wait("update_form_button")
+      assert peek(socket, :out) == %{"__undefined_0" => "textarea initial value", 
+                                    "__undefined_1" => "text1 initial value", 
+                                    "select_input" => "2", 
+                                    "text_input" =>  "text1 initial value"}
+    end
+
+    test "poking should update form" do
+      socket = drab_socket()
+      assert peek(socket, :out) == %{}
+      poke socket, text1: "text1 updated value",
+                   textarea1: "textarea updated value",
+                   select1: 3
+      click_and_wait("update_form_button")
+      assert peek(socket, :out) == %{"__undefined_0" => "textarea updated value", 
+                                    "__undefined_1" => "text1 updated value", 
+                                    "select_input" => "3", 
+                                    "text_input" => "text1 updated value"}
+    end
+
+  end
+end
