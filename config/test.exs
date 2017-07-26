34e7f72b08a0a1d709c1ddc86a35c15d8f139823
Tomek Gryszkiewicz <grych@tg.pl>
2017-07-26 15:42:28 +0200

    travis

--- config/test.exs
+++ config/test.exs
@@ -11,8 +11,8 @@ config :logger, level: :warn
 
 # phantomjs does not work correctly, shows some JS circular errors
 # config :hound, driver: "chrome_driver"
-config :hound, driver: "selenium"
-config :hound, browser: "chrome"
+# config :hound, driver: "selenium"
+# config :hound, browser: "chrome"
 # config :hound, driver: "phantomjs"
 
 # config :drab, 
---
+++ test/start_webdriver.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$WEBDRIVER" = "phantomjs" ]; then
+  npm install -g phantomjs
+  nohup phantomjs -w &
+  echo "Running with PhantomJs..."
+  sleep 3
+elif [ "$WEBDRIVER" = "selenium" ]; then
+  wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
+  nohup java -jar selenium-server-standalone-2.48.2.jar &
+  echo "Running with Selenium..."
+  sleep 10
+fi
