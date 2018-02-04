5c4f226be8b15be7c3c1e1dc5220b04b2c5f7991
Tomek Gryszkiewicz <grych@tg.pl>
2018-02-04 21:03:47 +0100

    exception

--- lib/drab/exceptions.ex
+++ lib/drab/exceptions.ex
@@ -2,5 +2,7 @@ defmodule Drab.JSExecutionError do
   @moduledoc """
   Raised when the browser encounters a JS error or the timeout for the current operation.
   """
+
+  @doc false
   defexception message: "JavaScript error"
 end
