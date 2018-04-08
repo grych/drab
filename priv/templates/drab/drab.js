d65f70045a48f88df9277181e8ccd180a51e5dfa
Tomek Gryszkiewicz <grych@tg.pl>
2018-04-08 19:20:22 +0200

    changed the drab-id generation

--- priv/templates/drab/drab.js
+++ priv/templates/drab/drab.js
@@ -1,15 +1,15 @@
 (function () {
   "use strict";
   function uuid() {
-    // borrowed from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = (d + Math.random() * 16) % 16 | 0;
-      d = Math.floor(d / 16);
-      return (c == 'x' ? r : r & 0x3 | 0x8).toString(16);
-    });
-    return uuid;
-    // return window.Drab.counter++;
+    // // borrowed from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    // var d = new Date().getTime();
+    // var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    //   var r = (d + Math.random() * 16) % 16 | 0;
+    //   d = Math.floor(d / 16);
+    //   return (c == 'x' ? r : r & 0x3 | 0x8).toString(16);
+    // });
+    // return uuid;
+    return window.Drab.counter++;
   }
 
   function closest(el, fn) {
